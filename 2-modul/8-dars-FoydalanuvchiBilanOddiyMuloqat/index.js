e184ab61bd8271007b757def560eb12bd38bbe0e
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-08-01 10:15:29 +0500

    Foydalanuvchi bilan oddiy muloqot

--- 2-modul/8-dars-FoydalanuvchiBilanOddiyMuloqat/index.js
+++ 2-modul/8-dars-FoydalanuvchiBilanOddiyMuloqat/index.js
@@ -3,5 +3,19 @@
 // const place = confirm("Do you want to delete?")
 // console.log(place);
 
-const currentPlace = prompt("Where are you from", "Earth")
-console.log(currentPlace);
+// const currentPlace = prompt("Where are you from", "Earth")
+// console.log(typeof currentPlace);
+
+// foydalanuvchidan olingan malumotlar doim string formatda keladi
+
+// typeni ozgartirish
+
+// const age = prompt("How old are you", "10")
+//     // console.log(typeof age); // + bilan o'zgartirish mumkin o'zgaruvchi oldiga + ishora qoyiladi
+// console.log(+age + 5);
+
+const favouriteColor = []
+favouriteColor[0] = "Red"
+favouriteColor[1] = prompt("what is your first name")
+favouriteColor[2] = prompt("what is your last name")
+console.log(favouriteColor);
