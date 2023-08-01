2fefff1b0975da752305b1216b28ab4564317fe4
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-08-01 10:07:19 +0500

    Foydalanuvchi bilan oddiy muloqot

---
+++ 2-modul/8-dars-FoydalanuvchiBilanOddiyMuloqat/index.js
@@ -0,0 +1,7 @@
+// alert("Hello alert")
+
+// const place = confirm("Do you want to delete?")
+// console.log(place);
+
+const currentPlace = prompt("Where are you from", "Earth")
+console.log(currentPlace);
