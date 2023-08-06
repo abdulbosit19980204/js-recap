e443b46a644c1ee6b6530dbcc7afd6f4306984f2
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-08-06 10:42:01 +0500

    Funktion declaration ES6

---
+++ 2-modul/16-dars-FunksiyaES6/main.js
@@ -0,0 +1,29 @@
+"use strict"
+
+console.log("Open Modal");
+
+function sayHelloworld() {
+    console.log("Hello world");
+}
+
+sayHelloworld()
+
+
+function fullName(name, surname) {
+    console.log(`My full name is ${name} ${surname}`);
+}
+
+fullName("Abdulbosit", "Tuychiev")
+
+
+const calc = function(a, b) {
+    return a + b
+}
+
+console.log(calc(5, 6));
+
+const additioner = (a, b) => {
+    return a + b
+}
+
+console.log(additioner(6, 7));
